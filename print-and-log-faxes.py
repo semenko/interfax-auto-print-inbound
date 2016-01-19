e385de2e6dce7109f33c1f6690cf73bcaf8fce5f
Nick Semenkovich <semenko@alum.mit.edu>
2016-01-19 17:07:53 -0600

    Indent 4

--- print-and-log-faxes.py
+++ print-and-log-faxes.py
@@ -17,29 +17,26 @@ import os
 from interfax import client
 from sys import stderr
 
-# Inbound storage
-try:
-     # This is all on a ram drive for security.
-     os.mkdir("inbound")
-except OSError:
-     pass
 
-print("Loading Getting credentials.")
+# Load settings
 config = ConfigParser.ConfigParser()
-config.read('.auth')
+config.read('interfax.yaml')
 
 username = config.get('interfax', 'username', 0)
 password = config.get('interfax', 'password', 0)
+inbound_directory = config.get('interfax', 'inbound')
+
+assert(os.access(inbound_directory, os.W_OK))
 
 print("Getting inbound cache...")
 try:
-     pkl_file = open('.pickle-cache', 'rb')
-     inbound_cache = pickle.load(pkl_file)
-     while len(inbound_cache) > 20:
-          inbound_cache.pop()
+    pkl_file = open('.pickle-cache', 'rb')
+    inbound_cache = pickle.load(pkl_file)
+    while len(inbound_cache) > 20:
+        inbound_cache.pop()
 except IOError:
-     print("\tNo cache found, creating new one.")
-     inbound_cache = []
+    print("\tNo cache found, creating new one.")
+    inbound_cache = []
 
 
 print("Connecting to Interfax")
@@ -52,23 +49,23 @@ c = client.InterFaxClient(username, password)
 print("Getting 20 most recent inbound faxes...")
 
 try:
-     in_result = c.getList('AllMessages', 20)
+    in_result = c.getList('AllMessages', 20)
 except socket.gaierror:
-     print("Endpoint error, let's try later.")
-     exit()
+    print("Endpoint error, let's try later.")
+    exit()
 except socket.error:
-     print("Other generic socket error. We'll try back later.")
-     exit()
+    print("Other generic socket error. We'll try back later.")
+    exit()
 except TypeError:
-     print("Endpoint error, returning HTML.")
-     exit()
+    print("Endpoint error, returning HTML.")
+    exit()
 
 if in_result[0] == -150:
-     print("Inbound internal error. Trying back later.")
-     exit()
+    print("Inbound internal error. Trying back later.")
+    exit()
 elif in_result[0] != 0:
-     print >> stderr, "ERROR: Inbound return code %d" % in_result[0]
-     exit()
+    print >> stderr, "ERROR: Inbound return code %d" % in_result[0]
+    exit()
 print('\tGetList returned with %d items' % (len(in_result[1])))
 
 
@@ -76,28 +73,28 @@ print('\tGetList returned with %d items' % (len(in_result[1])))
 # Loop over inbound faxes
 in_log = open('inbound_fax_log.txt', 'a')
 for in_item in reversed(in_result[1]):
-     # Sanity check
-     assert(int(in_item[0]) == in_item[0])
+    # Sanity check
+    assert(int(in_item[0]) == in_item[0])
 
-     if in_item[0] not in inbound_cache:
-          inbound_cache.insert(0, in_item[0])
+    if in_item[0] not in inbound_cache:
+        inbound_cache.insert(0, in_item[0])
 
-          # "\nmessageId: %d\nphoneNumber: %s\nremoteCSID: %s\nmessageStatus: %d\npages: %d\nmessageSize: %d\nmessageType: %d\nreceiveTime: %s\ncallerID: %s\nmessageRecodingDuration: %d"
-          dl_return_code = c.getImageChunk(in_item[0], True, in_item[5], 0, "inbound/%d.pdf" % in_item[0] )          
+        # "\nmessageId: %d\nphoneNumber: %s\nremoteCSID: %s\nmessageStatus: %d\npages: %d\nmessageSize: %d\nmessageType: %d\nreceiveTime: %s\ncallerID: %s\nmessageRecodingDuration: %d"
+        dl_return_code = c.getImageChunk(in_item[0], True, in_item[5], 0, "inbound/%d.pdf" % in_item[0] )
 
-          if dl_return_code == 0:
-               print("*** Printing %d" % (in_item[0]))
-               ### Call lpr and print
-               retcode = subprocess.call(['lpr', '-P', 'Brother_MFC-8910DW', 'inbound/%d.pdf' % in_item[0]])
-               if retcode < 0:
-                    print >>stderr, "Printing error, child was terminated by signal", -retcode
-          else:
-               print("Error downloading message %d, code: %d" % (in_item[0], dl_return_code))
+        if dl_return_code == 0:
+            print("*** Printing %d" % (in_item[0]))
+            ### Call lpr and print
+            retcode = subprocess.call(['lpr', '-P', 'Brother_MFC-8910DW', 'inbound/%d.pdf' % in_item[0]])
+            if retcode < 0:
+                print >>stderr, "Printing error, child was terminated by signal", -retcode
+        else:
+            print("Error downloading message %d, code: %d" % (in_item[0], dl_return_code))
 
-     # Write the log of inbound faxes.
-     t = in_item[7]
-     rcv_time = "%d/%d/%d %02d:%02d" % (t[1], t[2], t[0], t[3], t[4])  # Meh, strftime lazy. 
-     print >> in_log , "%s> From: %s (%s), Pages: %d, Message ID: %d\r" % (rcv_time, filter(str.isalnum, in_item[8]), filter(str.isalnum, in_item[2]), in_item[4], in_item[0])
+    # Write the log of inbound faxes.
+    t = in_item[7]
+    rcv_time = "%d/%d/%d %02d:%02d" % (t[1], t[2], t[0], t[3], t[4])  # Meh, strftime lazy.
+    print >> in_log , "%s> From: %s (%s), Pages: %d, Message ID: %d\r" % (rcv_time, filter(str.isalnum, in_item[8]), filter(str.isalnum, in_item[2]), in_item[4], in_item[0])
 
 in_log.close()
 
